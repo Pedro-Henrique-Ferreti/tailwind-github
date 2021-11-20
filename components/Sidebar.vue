2fdc5acf7326e2a6292406b62a607bdb1ab91cab
Pedro Henrique <phfs28@gmail.com>
2021-11-20 10:18:03 -0300

    Added about section

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="w-1/4">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsum esse quam assumenda eius doloribus, aspernatur nam placeat porro omnis excepturi reiciendis alias veritatis, est asperiores quasi sed possimus eligendi officiis, quod voluptatum debitis in a ducimus. Voluptas eligendi aut mollitia vel doloremque itaque distinctio corporis explicabo maxime deserunt quae delectus asperiores, neque repellat. Ad voluptates autem eligendi laborum, facilis blanditiis cupiditate. Similique voluptas necessitatibus blanditiis ex atque deserunt reprehenderit illo cupiditate ea in. Aperiam officiis minima, illo autem ex fuga hic, vel delectus qui ratione veritatis aspernatur mollitia blanditiis. Voluptatem, neque quod! Laboriosam, ipsam! Error culpa aperiam officia. Ipsa!
+    <h4 class="text-base font-semibold">About</h4>
+    <p class="mt-4 italic text-gray-600">No description, website, or topics provided.</p>
   </div>
 </template>
 
