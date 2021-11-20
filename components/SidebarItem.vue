422213724a9b9a41209c64a086f7885c9790cee0
Pedro <phfs28@gmail.com>
2021-11-20 19:54:32 -0300

    Created SidebarItem

---
+++ components/SidebarItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="py-6 border-b border-gray-300">
+    <div class="flex items-center justify-between">
+      <h4 class="text-base font-semibold">{{ title }}</h4>
+      <slot name="header" />
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarItem',
+  props: {
+    title: String,
+  },
+};
+</script>
